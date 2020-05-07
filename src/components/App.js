22293c5ca89033491c57371ffa81cdca87522874
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-05-07 13:35:54 -0600

    Code for 42-structure-components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import * as React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-export default function App () {
-  return (
-    <div>
-      Goals - Todos
-    </div>
-  )
+class App extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    dispatch(handleInitialData())
+  }
+  render() {
+    const { loading } = this.props
+
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
+
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    )
+  }
 }
+
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
