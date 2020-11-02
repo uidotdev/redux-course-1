6126d3665c0514e9d00733ef143c7f02b0872fd9
Bilge Demirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-11-02 11:37:22 +0300

    mistake in createStore function

--- index.js
+++ index.js
@@ -24,32 +24,6 @@ function createStore (reducer) {
   }
 
   return {
-// Library code
-function createStore (reducer) {
-  // The store should have four parts
-  // 1. The state
-  // 2. Get the state. (getState)
-  // 3. Listen to changes on the state. (subscribe)
-  // 4. Update the state (dispatch)
-
-  let state
-  let listeners = []
-
-  const getState = () => state
-
-  const subscribe = (listener) => {
-    listeners.push(listener)
-    return () => {
-      listeners = listeners.filter((l) => l !== listener)
-    }
-  }
-
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-
-  return {
     getState,
     subscribe,
     dispatch,
